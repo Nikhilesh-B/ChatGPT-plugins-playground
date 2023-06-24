f43affecb92c90b4189c98bc4ba7e0e74533cb64
Nikhilesh Belulkar <nb2953@columbia.edu>
2023-06-24 14:49:59 -0400

    pushing the basic backend too

---
+++ password_reset/.well_known/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import random
+
+app = Flask(__name__)
+
+
+FNAME, LNAME, ID = 'NIKHILESH', 'BELULKAR', 2
+animals = ['Giraffe', 
+           'Snake', 
+           'Lion', 
+           'Tiger', 
+           'Gorilla', 
+           'Fox', 
+           'Kangaroo', 
+           'Squirrel', 
+           'Elephant', 
+           'Crocodile']
+
+
+@app.route('/submit-verification-information', methods=['POST'])
+def verify_information():
+    user_information = request.get_json()
+    fname= user_information["first_name"]
+    lname= user_information["last_name"]
+    idnumber= user_information["id_number"]
+
+    if fname.upper()==FNAME and lname.upper()==LNAME and idnumber == ID:
+        new_password = random.choice(animals)+str(random.randint(0,100))
+        robj = {"return_string":"Your new password is "+new_password}
+
+        return jsonify(robj)

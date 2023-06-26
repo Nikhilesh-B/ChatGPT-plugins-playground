ae4a4466e77d562a0adc018f581fc5f3ef0e82e6
Nikhilesh Belulkar <nb2953@columbia.edu>
2023-06-26 17:54:56 -0400

    going to work at least we can try it?

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import random
+from flask_cors import CORS
+import subprocess
+import os
+
 
 app = Flask(__name__)
+CORS(app)
 
+well_known_dir = os.path.join(app.root_path, '.well-known')
 
 FNAME, LNAME, ID = 'NIKHILESH', 'BELULKAR', 2
 animals = ['Giraffe', 
@@ -17,17 +23,24 @@ animals = ['Giraffe',
            'Crocodile']
 
 
+@app.route('/.well-known/<path:filename>')
+def well_known(filename):
+    return send_from_directory(well_known_dir, filename)
+
+
 @app.route('/submit-verification-information', methods=['POST'])
 def verify_information():
     user_information = request.get_json()
-    fname= user_information["first_name"]
-    lname= user_information["last_name"]
-    idnumber= user_information["id_number"]
+    fname = user_information["first_name"]
+    lname = user_information["last_name"]
+    idnumber = user_information["id_number"]
 
     if fname.upper()==FNAME and lname.upper()==LNAME and idnumber == ID:
         new_password = random.choice(animals)+str(random.randint(0,100))
         robj = {"return_string":"Your new password is "+new_password}
         return jsonify(robj)
+    
+    return {}
     
 
 if __name__ == "__main__":
